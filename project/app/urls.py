9d9636a1182d8ba50b631c0ef64f2311c184d6ee
Rahul Lokhande <rlokhande06244@gmail.com>
2024-10-24 14:56:41 +0530

    Table Relations (Forms)

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('userdata/',views.userdata,name='userdata'),
+    # path('profiledata/',views.profiledata,name='profiledata')
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -9,3 +9,9 @@ def home(request):
     context['profile']=ProfileForm
 
     return render (request,'home.html',context)
+
+def userdata(request):
+    if request.method=="POST":
+        form=UserForm(request.POST)
+        if form.is_valid():
+            form.save()
