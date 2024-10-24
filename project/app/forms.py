b838baa5b01936a02f8bd40ae99a35aed40a9dc1
Rahul Lokhande <rlokhande06244@gmail.com>
2024-10-24 13:39:41 +0530

    Table Relations (Forms)

--- project/app/forms.py
+++ project/app/forms.py
@@ -5,8 +5,3 @@ class UserForm(forms.ModelForm):
     class Meta:
         model=User
         fields='__all__'
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model=Profile
-        fields='__all__'
